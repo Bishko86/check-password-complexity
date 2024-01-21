5780dd4db2bac485fff0195edffbcc0f6cc00037
Roman Bishko <rbish@softserveinc.com>
2024-01-21 16:44:47 +0200

    create base component

---
+++ src/app/check-password-complexity/check-password-complexity.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check-password-complexity',
+  standalone: true,
+  imports: [],
+  templateUrl: './check-password-complexity.component.html',
+  styleUrl: './check-password-complexity.component.scss'
+})
+export class CheckPasswordComplexityComponent {
+
+}
