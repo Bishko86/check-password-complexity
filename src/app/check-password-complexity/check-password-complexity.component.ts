440540b468c82c4a54c4ee5b16c6aa258e2b2217
Roman Bishko <rbish@softserveinc.com>
2024-01-22 11:57:30 +0200

    create check pass component; add basic layout

--- src/app/check-password-complexity/check-password-complexity.component.ts
+++ src/app/check-password-complexity/check-password-complexity.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { DebounceEventDirective } from '../directives/debounce-event.directive';
+import { PasswordService } from '../services/password.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-check-password-complexity',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, DebounceEventDirective, NgClass],
   templateUrl: './check-password-complexity.component.html',
   styleUrl: './check-password-complexity.component.scss'
 })
 export class CheckPasswordComplexityComponent {
+  form = { password: '' };
+  passwordState = this.passwordService.definePasswordComplexity('');
 
+  constructor(
+    private readonly passwordService: PasswordService,
+    
+  ) {}
+
+  changePassword(): void {
+    this.passwordState = this.passwordService.definePasswordComplexity(this.form.password.trim());
+
+  }
 }
