15d34572ca2354835efba4320f931e2cf534c9cf
Roman Bishko <rbish@softserveinc.com>
2024-01-22 13:45:00 +0200

    create password layout and component logic

--- src/app/check-password-complexity/check-password-complexity.component.ts
+++ src/app/check-password-complexity/check-password-complexity.component.ts
@@ -1,28 +1,31 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
 
-import { DebounceEventDirective } from '../directives/debounce-event.directive';
 import { PasswordService } from '../services/password.service';
-import { NgClass } from '@angular/common';
+import { PasswordState } from '../interfaces/password-state.interface';
 
 @Component({
   selector: 'app-check-password-complexity',
   standalone: true,
-  imports: [FormsModule, DebounceEventDirective, NgClass],
+  imports: [NgClass, NgIf],
   templateUrl: './check-password-complexity.component.html',
-  styleUrl: './check-password-complexity.component.scss'
+  styleUrl: './check-password-complexity.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CheckPasswordComplexityComponent {
-  form = { password: '' };
-  passwordState = this.passwordService.definePasswordComplexity('');
+  isFieldEmpty = true;
+  hasErrors = false;
+  passwordState: PasswordState = this.passwordService.definePasswordComplexity('');
 
   constructor(
     private readonly passwordService: PasswordService,
-    
   ) {}
 
-  changePassword(): void {
-    this.passwordState = this.passwordService.definePasswordComplexity(this.form.password.trim());
+  changePassword(event: Event): void {
+    const password = this.passwordService.getPassword(event);
 
+    this.isFieldEmpty = this.passwordService.isFieldEmpty(password);
+    this.hasErrors = this.passwordService.hasErrors(password);
+    this.passwordState = this.passwordService.definePasswordComplexity(password);
   }
 }
