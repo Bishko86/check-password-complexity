909fd550f87487fc4664e5902e41cbca01ed74b7
Roman Bishko <rbish@softserveinc.com>
2024-01-22 13:46:35 +0200

    add regexes to handle password complexity

---
+++ src/app/constants/password-regex.constant.ts
@@ -0,0 +1,6 @@
+export const LettersRegex = /[a-zA-Z]/;
+export const DigitsRegex = /\d/;
+export const SymbolsRegex = /[^a-zA-Z0-9]/;
+export const LengthStringRegex = /^.{8,}$/;
+export const EmptyStringRegex = /^$/;
+export const LatinDigitsSymbolsRegex = /^[a-zA-Z0-9!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]*$/;
